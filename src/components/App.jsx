9ccae76311cc92ab4591667d742c29c4f14e5c38
Kudese <d.kuprichenkov@gmail.com>
2023-01-06 03:40:41 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Contacts from './Contacts/Contacts';
+import PhoneBook from './PhoneBook/PhoneBook';
+
+export class App extends Component() {
+  // state = {
+  //   contacts: [],
+  //   name: '',
+  // };
+  render() {
+
+    return (
+      <>
+        <PhoneBook />
+        <Contacts />
+      </>
+    );
+  }
+}
